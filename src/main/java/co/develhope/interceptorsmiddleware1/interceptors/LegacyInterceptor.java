eeec52eb1aae003820305e593100881f48a5b767
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-30 16:56:41 +0200

    first commit

---
+++ src/main/java/co/develhope/interceptorsmiddleware1/interceptors/LegacyInterceptor.java
@@ -0,0 +1,28 @@
+package co.develhope.interceptorsmiddleware1.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class LegacyInterceptor implements HandlerInterceptor {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getRequestURI().equals("/legacy")) {
+            response.setStatus(HttpStatus.GONE.value());
+            response.getWriter().write("L'endpoint non è più disponibile");
+            return false;
+        }
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
