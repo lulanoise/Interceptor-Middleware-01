ca7b074af07bb2a177c6349955f90f49dae6ccaf
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-30 17:01:12 +0200

    modified logging api

--- src/main/java/co/develhope/interceptorsmiddleware1/interceptors/APILoggingInterceptor.java
+++ src/main/java/co/develhope/interceptorsmiddleware1/interceptors/APILoggingInterceptor.java
@@ -2,6 +2,8 @@ package co.develhope.interceptorsmiddleware1.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -9,22 +11,13 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class APILoggingInterceptor implements HandlerInterceptor {
-
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-      // String userAgent = request.getHeader("User-Agent");
-       System.out.println("User-Agent: " + request.getHeader("User-Agent"));
-        return true;
-    }
-
-    /*
-      private static final Logger LOGGER = LoggerFactory.getLogger(APILoggingInterceptor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(APILoggingInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         LOGGER.info("User-Agent: {}", request.getHeader("User-Agent"));
         return true;
     }
-     */
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
     }
